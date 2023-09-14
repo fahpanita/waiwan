31c86b911bc2485ab9150d1ae0a20c9d26f0a461
fahh <fahpanita86@gmail.com>
2023-09-14 20:36:09 +0700

    newver

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Routing from './Routing'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,20 +2,61 @@ import React from "react";
 import styled from "styled-components";
 import AuthenticatedProvider from "../../Providers/AuthenticatedProvider";
 import LineLogin from "./LineLogin";
-import { Image } from 'antd';
 import Link from "../../components/Link";
+import { Input, Space, Image, Select } from 'antd';
+
+const { Search } = Input;
+const onSearch = (value, _e, info) => console.log(info?.source, value);
+const handleChange = (value) => {
+    console.log(`selected ${value}`);
+};
 
 const Home = () => {
     return (
         <AuthenticatedProvider>
             <Container>
-                <div className="abc">123</div>
-                <Img src="image/icons/line_2k.svg" />
-                <Link to="/stock">Blogs</Link>
-                <Imgrounder
+                <div className="abc">ไหว้วาน</div>
+                <Img src="image/img/banner-1.png" />
+                <div className="row">
+                    <div className="col-md-2">
+                        <Select
+                            defaultValue="lucy"
+                            style={{
+                                width: "100%",
+                            }}
+                            onChange={handleChange}
+                            options={[
+                                {
+                                    value: 'jack',
+                                    label: 'Jack',
+                                },
+                                {
+                                    value: 'lucy',
+                                    label: 'Lucy',
+                                },
+                                {
+                                    value: 'Yiminghe',
+                                    label: 'yiminghe',
+                                },
+                            ]}
+                        />
+                    </div>
+                    <div className="col col-md-10">
+                        <Search placeholder="input search text" onSearch={onSearch} enterButton />
+                    </div>
+                </div>
+                <Icon src="image/icons/fire-left.png" />
+                <div className="font-36">เทรนด์นิยมช่วงนี้</div>
+                <div className="center"><Card src="image/img/card-1.png" />
+                    <Card src="image/img/card-2.png" />
+                    <Card src="image/img/card-1.png" /></div>
+
+                {/* <Icon src="image/icons/line_2k.svg" />
+                <Link to="/stock">Blogs</Link> */}
+                {/* <Imgrounder
                     width={200}
                     src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-                />
+                /> */}
                 <LineLogin />
             </Container>
         </AuthenticatedProvider>
@@ -23,15 +64,31 @@ const Home = () => {
 };
 
 export const Container = styled.div`
-    max-width: 870px;
+    max-width: 1440px;
     margin: 0 auto;
     .abc {
         font-size: 50px;
+        text-align: center;
     }
+    .font-36 {
+        font-family: 'Athiti', sans-serif;
+        font-size: 36px;
+    }
+   
 `;
 
 export const Img = styled.img`
-    width: 20px;
+    width: 100%;
+`;
+
+export const Card = styled.img`
+    width: 32%;
+    float: left;
+`;
+
+export const Icon = styled.img`
+    width: 36px;
+    float: left;
 `;
 
 export const Imgrounder = styled(Image)`
