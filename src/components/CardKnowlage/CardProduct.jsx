f0290efe56ff74e689587bec84d32e97c7845976
Natthanan Vaowkhajon <vaowkhajon_n@silpakorn.edu>
2023-10-18 22:15:18 +0700

    Add front-home

--- src/components/CardKnowlage/CardProduct.jsx
+++ src/components/CardKnowlage/CardProduct.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
 
 const CardProduct = () => {
   return (
     <Card
     // style={{ width: "18rem" }}
     >
-      <Card.Img variant="top" src="image/img/product-01.png" />
+      <Card.Img variant="top" src="image/img/product-01.png"></Card.Img>
       <Card.Body>
         <Card.Title>ซาลาเปา6ลูก</Card.Title>
-        <Card.Text>฿139</Card.Text>
-        {/* <Button variant="primary">ซื้อสินค้า</Button> */}
+        <Card.Text>฿{139}</Card.Text>
+        <Link to="/detailProduct">
+          <Button>ดูเพิ่มเติม</Button>
+        </Link>
       </Card.Body>
     </Card>
   );
